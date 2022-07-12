c4ba3c861354b5897c20813091aec64fe5b34c54
YasithCB <yasith.cb2001@gmail.com>
2022-07-12 09:49:35 +0530

    DashboardForm created

---
+++ src/AppInitializer.java
@@ -0,0 +1,27 @@
+/**
+ * author  Yasith C Bandara
+ * created 7/12/2022 - 9:38 AM
+ * project DBP-Final
+ */
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/DashboardForm.fxml"))));
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
---
+++ src/controller/DashboardFormController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+
+/**
+ * author  Yasith C Bandara
+ * created 7/12/2022 - 9:38 AM
+ * project DBP-Final
+ */
+
+public class DashboardFormController {
+    public JFXButton btnStudent;
+
+    public void initialize(){
+        btnStudent.setOnMouseClicked(event -> {
+
+        });
+    }
+}
